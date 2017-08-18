6318ed147f6133b98caba1003f5c715acd187903
voronchikhin <voronchikhin@haulmont.com>
2017-08-18 11:26:52 +0400

    stage 2

--- flask-microservices-users/manage.py
+++ flask-microservices-users/manage.py
@@ -1,9 +1,22 @@
 import unittest
+import coverage
 
 from flask_script import Manager
 
 from project import create_app, db
 from project.api.models import User
+
+
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/server/config.py',
+        'project/server/*/__init__.py'
+    ]
+)
+COV.start()
 
 
 app = create_app()
@@ -21,6 +34,22 @@ def test():
 
 
 @manager.command
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
+
+@manager.command
 def recreate_db():
     """Recreates a database."""
     db.drop_all()
@@ -33,6 +62,7 @@ def seed_db():
     db.session.add(User(username='michael', email="michael@realpython.com"))
     db.session.add(User(username='michaelherman', email="michael@mherman.org"))
     db.session.commit()
+
 
 if __name__ == '__main__':
     manager.run()
